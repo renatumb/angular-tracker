1ffa26db8952b752defdd024f491524ea24db542
RENATU BONFIM <renatu_mb@yahoo.com.br>
2025-11-04 12:20:06 -0800

    Mock Data

---
+++ src/app/Tasks.ts
@@ -0,0 +1,8 @@
+interface Task {
+  id?: number;
+  text: string;
+  day: Date;
+  reminder: boolean
+}
+
+export default Task;
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,31 @@
+import Tasks from './Tasks';
+
+export const mockTasks: Tasks[] = [
+  {
+    id: 1,
+    text: 'text 1',
+    day: new Date('2011/10/01'),
+    reminder: true
+  },
+
+  {
+    id: 3,
+    text: 'text 3',
+    day: new Date('2013/10/03'),
+    reminder: true
+  },
+
+  {
+    id: 5,
+    text: 'text 5',
+    day: new Date('2015/10/05'),
+    reminder: true
+  },
+
+  {
+    id: 7,
+    text: 'text 7',
+    day: new Date('2017/10/07'),
+    reminder: true
+  }
+]
