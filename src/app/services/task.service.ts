cf1ee9ddc1d7dc0456e50e6faeb645ed5baf9007
RENATU BONFIM <renatu_mb@yahoo.com.br>
2025-11-04 15:13:49 -0800

    Use http calls

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,18 +1,23 @@
 import {Injectable} from '@angular/core';
 
-import {mockTasks} from '../data_json/mock-tasks';
+// import {mockTasks} from '../data_json/mock-tasks';
 import Tasks from '../interfaces/Tasks';
 import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() {
+  private API_URL = 'http://localhost:5000/tasks';
+
+  constructor(private httpHandler: HttpClient) {
   }
 
+
   public getTasks(): Observable<Tasks[]> {
-    return of(mockTasks);
+//  return of(mockTasks);
+    return this.httpHandler.get<Tasks[]>(`${this.API_URL}/`);
   }
 }
